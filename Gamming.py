273fddc9ee68ebdfb37648df19ba22a8cf3244ce
EITH SENGHIM <73463540+EITHSENGHIM@users.noreply.github.com>
2021-03-02 15:56:10 +0700

    My code are many line

--- Gamming.py
+++ Gamming.py
@@ -1,38 +1,42 @@
 # IMPORTS
 import tkinter as tk
 import random
+import winsound
 from tkinter import *
 
 # CONSTANTS
-PLAYER  = 1
-EMPTY = 0
+ScoreText = 0
 moveX = 725
 moveY = 175
-moveX1 = 80
+moveX1 = 60
 moveY1 = 300
 moveX2 = 370
 moveY2 = 180
 moveX3 = 430
 moveY3 = 480
+YouWin = True
 returnBack = True
 returnBack1 = True
 returnBack2 = True
 returnBack3 = True
+TouchEmemy = True
 SCREEN_WIDTH = 1015
 SCREEN_HEIGHT = 700
-SCREEN_TITLE = "You can Right, Left, Up, and Down."
+SCREEN_TITLE = "You can use Keys Right, Left, Up, and Down."
 
 # VARIABLES
 number_entiVirus = 0
+TouchEmemy1 = 0
+TouchEmemy2 = 0
 
 grid = [
 [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
-[3,1,0,0,2,3,0,0,0,0,0,3,0,2,3],
-[3,0,3,0,3,2,0,0,3,0,3,0,0,3,3],
-[3,0,0,0,0,3,0,3,2,0,0,0,3,2,3],
-[3,3,0,3,0,0,0,3,2,0,3,0,0,0,3],
+[3,1,0,0,2,3,4,4,4,4,4,3,0,2,3],
+[3,0,3,0,3,2,0,0,3,0,3,4,0,3,3],
+[3,4,4,4,4,3,0,3,2,0,0,4,3,2,3],
+[3,3,0,3,2,0,0,3,2,0,3,4,0,0,3],
 [3,0,0,0,3,0,0,0,3,0,0,3,0,0,3],
-[3,2,3,0,0,0,3,0,0,0,0,0,3,2,3],
+[3,2,3,0,0,2,3,4,4,4,4,4,3,2,3],
 [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]]
 x1 = 0
 x2 = 0
@@ -42,7 +46,7 @@ y2 = 0
 
 # draw grid as ractangle
 def arrayToDrawing():
-    global x1, x2, y1, y2
+    global x1, x2, y1, y2, TouchEmemy1, TouchEmemy2
     for rowGRid in range(len(grid)) :
         for colGrid in range(len(grid[rowGRid])):
             x1 = (colGrid * 60)
@@ -52,14 +56,8 @@ def arrayToDrawing():
             if grid[rowGRid][colGrid]==1 :
                 canvas.create_rectangle(60+x1,115+y1,60+x2,115+y2,fill="", outline="blue")
                 canvas.create_image(67+x1,60+y2, image=imageDoctor, anchor=NW)
-            elif colGrid==11 and rowGRid>1 and rowGRid<len(grid)-3 :
-                    canvas.create_rectangle(60+x1,115+y1,60+x2,115+y2,fill="", outline="blue")
-            elif colGrid>0 and colGrid<5 and rowGRid==3 :
-                    canvas.create_rectangle(60+x1,115+y1,60+x2,115+y2,fill="", outline="blue")
-            elif colGrid>5 and colGrid<11 and rowGRid==1 :
-                    canvas.create_rectangle(60+x1,115+y1,60+x2,115+y2,fill="", outline="blue")
-            elif colGrid>6 and colGrid<12 and rowGRid==6 :
-                    canvas.create_rectangle(60+x1,115+y1,60+x2,115+y2,fill="", outline="blue")
+            elif grid[rowGRid][colGrid]==4 :
+                canvas.create_rectangle(60+x1,115+y1,60+x2,115+y2,fill="", outline="blue")
             elif grid[rowGRid][colGrid]==2 :
                 canvas.create_rectangle(60+x1,115+y1,60+x2,115+y2,fill="", outline="blue")
                 canMove3 = canvas.create_image(67+x1,60+y2, image=entiVirus, anchor=NW)
@@ -67,164 +65,227 @@ def arrayToDrawing():
                 canvas.create_rectangle(60+x1,115+y1,60+x2,115+y2,fill="red", outline="blue")
                 canvas.create_image(67+x1,60+y2, image=imageWall, anchor=NW) 
             else:
-                canvas.create_rectangle(60+x1,115+y1,60+x2,115+y2,fill="#7851ae", outline="blue")
+                canvas.create_rectangle(60+x1,115+y1,60+x2,115+y2,fill="", outline="blue")
+                
             
 def ememyMove0() :
-    global canMove0, moveX, moveY, returnBack
+    global canMove0, moveX, moveY, returnBack, TouchEmemy1, TouchEmemy2, TouchEmemy, YouWin
     canvas.moveto(canMove0, moveX,moveY)
     
-    if moveY<=380 and returnBack :
-        canvas.after(99, lambda:ememyMove0())
+    if moveY<=420 and returnBack and YouWin:
+        canvas.after(30, lambda:ememyMove0())
         moveY += 5
-        if moveY==380 :
+        if moveY==420 :
             returnBack = False
-    elif moveY>220 and not returnBack :
-        canvas.after(99, lambda:ememyMove0())
+        # if TouchEmemy1+5==moveX and (TouchEmemy2-165==moveY or TouchEmemy2-115==moveY or TouchEmemy2-65==moveY or TouchEmemy2-15==moveY) :
+            # TouchEmemy = False
+    elif moveY>180 and not returnBack and YouWin:
+        canvas.after(30, lambda:ememyMove0())
         moveY -= 5
-        if moveY==220 :
+        if moveY==180 :
             returnBack = True
+        # if TouchEmemy1+5==moveX and (TouchEmemy2-165==moveY or TouchEmemy2-115==moveY or TouchEmemy2-65==moveY or TouchEmemy2-15==moveY) :
+            # TouchEmemy = False
+    # print(moveX, moveY, TouchEmemy1+5, TouchEmemy2)
 def ememyMove1() :
-    global returnBack1, moveX1, moveY1, canMove1
+    global returnBack1, moveX1, moveY1, canMove1, TouchEmemy1, TouchEmemy2, TouchEmemy, YouWin
     canvas.moveto(canMove1, moveX1,moveY1)
-    if moveX1<=320 and returnBack1 :
-        canvas.after(80, lambda:ememyMove1())
+    if moveX1<=320 and returnBack1 and YouWin:
+        canvas.after(25, lambda:ememyMove1())
         moveX1 += 5
         if moveX1==320 :
             returnBack1 = False
-    elif moveX1>105 and not returnBack1 :
-        canvas.after(80, lambda:ememyMove1())
+        # if 120==moveX1 or 170==moveX1 or 220==moveX1 or 370 :
+            # TouchEmemy = False
+    elif moveX1>105 and not returnBack1 and YouWin:
+        canvas.after(30, lambda:ememyMove1())
         moveX1 -= 5
         if moveX1==105 :
             returnBack1 = True
+        # if 120==moveX1 or 170==moveX1 or 220==moveX1 or 370 :
+            # TouchEmemy = False
 def ememyMove2() :
-    global returnBack2, moveX2, moveY2, canMove2
+    global returnBack2, moveX2, moveY2, canMove2, TouchEmemy1, TouchEmemy2, TouchEmemy, YouWin
     canvas.moveto(canMove2, moveX2,moveY2)
-    if moveX2<=680 and returnBack2 :
-        canvas.after(60, lambda:ememyMove2())
-        moveX2 += 5
+    if moveX2<=680 and returnBack2 and YouWin:
+        canvas.after(10, lambda:ememyMove2())
+        moveX2 += 2
         if moveX2==680 :
             returnBack2 = False
-    elif moveX2>=400 and not returnBack2 :
-        canvas.after(60, lambda:ememyMove2())
-        moveX2 -= 5
+        # if TouchEmemy1+160>moveX2 or TouchEmemy1+210<moveX2 or TouchEmemy1+260>moveX2 or TouchEmemy1+310<moveX2 and TouchEmemy2==moveY2+175  :
+            # TouchEmemy = False
+    elif moveX2>=400 and not returnBack2 and YouWin :
+        canvas.after(10, lambda:ememyMove2())
+        moveX2 -= 2
         if moveX2==400 :
             returnBack2 = True
+            
+        # if TouchEmemy1+160==moveX2 or TouchEmemy1+210==moveX2 or TouchEmemy1+260==moveX2 or TouchEmemy1+310==moveX2 and TouchEmemy2==moveY2+175 :
+            # TouchEmemy = False
+        
 def ememyMove3() :
-    global returnBack3, moveX3, moveY3, canMove3
+    global returnBack3, moveX3, moveY3, canMove3, TouchEmemy1, TouchEmemy2, TouchEmemy, YouWin
     canvas.moveto(canMove3, moveX3,moveY3)
-    if moveX3<=755 and returnBack3 :
-        canvas.after(60, lambda:ememyMove3())
+    if moveX3<=755 and returnBack3 and YouWin :
+        canvas.after(30, lambda:ememyMove3())
         moveX3 += 5
         if moveX3==755 :
             returnBack3 = False
-    elif moveX3>=460 and not returnBack3 :
-        canvas.after(60, lambda:ememyMove3())
+        # if TouchEmemy1==moveX3 and TouchEmemy2==moveY3 :
+            # TouchEmemy = False
+    elif moveX3>=460 and not returnBack3 and YouWin :
+        canvas.after(30, lambda:ememyMove3())
         moveX3 -= 5
         if moveX3==460 :
             returnBack3 = True
+        # if TouchEmemy1==moveX3 and TouchEmemy2==moveY3 :
+            # TouchEmemy = False
 def findPlayerPosition(grid) :
     for i in range(len(grid)):
         for n in range(len(grid[i])):
-            if grid[i][n]== 1:
+            if grid[i][n]== 1 :
                 position=[i,n]
     return position
 def moveRight(event):
-    global position, grid, canMove0, canMove1, canMove2, canMove3, number_entiVirus
+    global position, grid, canMove0, canMove1, canMove2, canMove3, number_entiVirus, TouchEmemy, YouWin , TouchEmemy, ScoreText
     position = findPlayerPosition(grid)
     p_column = position[1]
     p_row = position[0]
-    if grid[p_row][p_column+1] ==0  :
+    if grid[p_row][p_column+1] ==0 and YouWin :
         grid[p_row][p_column]=0
         grid[p_row][p_column+1]=1
-    elif grid[p_row][p_column+1]==2 :
+    elif grid[p_row][p_column+1]==2 and YouWin :
         grid[p_row][p_column]=0
         grid[p_row][p_column+1]=1
+        ScoreText += 10
         number_entiVirus += 1
-
+    elif grid[p_row][p_column+1]==4 and YouWin :
+        grid[p_row][p_column]=0
+        grid[p_row][p_column+1]=1
+        
+        TouchEmemy = False
     canvas.delete("all")
     backgound_windows=canvas.create_image(500,500,image=bg)
     canMove0 = canvas.create_image(725,175, image=imageVirus0, anchor=NW)
-    canMove1 = canvas.create_image(80,300, image=imageVirus1, anchor=NW)
+    canMove1 = canvas.create_image(60,300, image=imageVirus1, anchor=NW)
     canMove2 = canvas.create_image(370,180, image=imageVirus2, anchor=NW)
     canMove3 = canvas.create_image(430,480, image=imageVirus3, anchor=NW)
-    if number_entiVirus==8 :
-        canvas.create_text(470,50, text="YOU WON.",fill="green", font=("Purisa", 35,"bold"))
-    canvas.create_text(530,630, text="If you can kills many Virus on the time, you win else lost.",fill="blue", font=("Purisa", 18))
-    canvas.create_text(500,660, text="It is follow level be level.",fill="blue", font=("Purisa", 18))
+    if number_entiVirus==10 :
+        canvas.create_text(500,60, text="YOU WON!!!",fill="white", font=("Purisa", 50,"bold"))
+        sd_Win()
+        YouWin = False
+    else :
+        canvas.create_text(500,50, text="Enjoy to play without lost.",fill="white", font=("Purisa", 20,"bold"))
+    canvas.create_text(510,630, text="If you can pick up many AntiVirus on the grid equal 100/100, you win.",fill="blue", font=("Purisa", 18))
+    canvas.create_text(510,660, text="I am sorry to player this game is easy player becuase you cannot lose.",fill="blue", font=("Purisa", 18))
+    canvas.create_text(170,90, text="Scores : " + str(ScoreText) + "/100",fill="yellow", font=("Purisa", 18))
+    sd_RightLeftUpDown()
     arrayToDrawing()
     
 def moveLeft(event):
-    global position, grid, canMove0, canMove1, canMove2, canMove3, number_entiVirus
+    global position, grid, canMove0, canMove1, canMove2, canMove3, number_entiVirus, TouchEmemy, YouWin, TouchEmemy, ScoreText
     position = findPlayerPosition(grid)
     p_column = position[1]
     p_row = position[0]
-    if grid[p_row][p_column-1]==0:
+    if grid[p_row][p_column-1]==0 and YouWin :
         grid[p_row][p_column]=0
         grid[p_row][p_column-1]=1
-    if grid[p_row][p_column-1]==2:
+    elif grid[p_row][p_column-1]==2 and YouWin :
         grid[p_row][p_column]=0
         grid[p_row][p_column-1]=1
+        ScoreText += 10
         number_entiVirus += 1
+    elif grid[p_row][p_column-1]==4 and YouWin :
+        grid[p_row][p_column]=0
+        grid[p_row][p_column-1]=1
+        TouchEmemy = False
     canvas.delete("all")
     backgound_windows=canvas.create_image(500,500,image=bg)
     canMove0 = canvas.create_image(725,175, image=imageVirus0, anchor=NW)
-    canMove1 = canvas.create_image(80,300, image=imageVirus1, anchor=NW)
+    canMove1 = canvas.create_image(60,300, image=imageVirus1, anchor=NW)
     canMove2 = canvas.create_image(370,180, image=imageVirus2, anchor=NW)
     canMove3 = canvas.create_image(430,480, image=imageVirus3, anchor=NW)
-    if number_entiVirus==8 :
-        canvas.create_text(470,50, text="YOU WON.",fill="green", font=("Purisa", 35,"bold"))
-    canvas.create_text(530,630, text="If you can kills many Virus on the time, you win else lost.",fill="blue", font=("Purisa", 18))
-    canvas.create_text(500,660, text="It is follow level be level.",fill="blue", font=("Purisa", 18))
+    if number_entiVirus==10 :
+        canvas.create_text(500,60, text="YOU WON!!!",fill="white", font=("Purisa", 50,"bold"))
+        sd_Win()
+        YouWin = False
+    else :
+        canvas.create_text(500,50, text="Enjoy to play without lost.",fill="white", font=("Purisa", 20,"bold"))
+    canvas.create_text(510,630, text="If you can pick up many AntiVirus on the grid equal 100/100, you win.",fill="blue", font=("Purisa", 18))
+    canvas.create_text(510,660, text="I am sorry to player this game is easy player becuase you cannot lose.",fill="blue", font=("Purisa", 18))
+    canvas.create_text(170,90, text="Scores : " + str(ScoreText) + "/100",fill="yellow", font=("Purisa", 18))
+    sd_RightLeftUpDown()
     arrayToDrawing()
     
 
 def moveUp(event):
-    global position, grid, canMove0, canMove1, canMove2, canMove3, number_entiVirus
+    global position, grid, canMove0, canMove1, canMove2, canMove3, number_entiVirus, TouchEmemy, YouWin, TouchEmemy, ScoreText
     position = findPlayerPosition(grid)
     p_column = position[1]
     p_row = position[0]
-    if grid[p_row-1][p_column]==0:
+    if grid[p_row-1][p_column]==0 and YouWin :
         grid[p_row][p_column]=0
         grid[p_row-1][p_column]=1
-    if grid[p_row-1][p_column]==2:
+    elif grid[p_row-1][p_column]==2 and YouWin :
         grid[p_row][p_column]=0
         grid[p_row-1][p_column]=1
+        ScoreText += 10
         number_entiVirus += 1
+    elif grid[p_row-1][p_column]==4 and YouWin :
+        grid[p_row][p_column]=0
+        grid[p_row-1][p_column]=1
+        TouchEmemy = False
     canvas.delete("all")
     backgound_windows=canvas.create_image(500,500,image=bg)
     canMove0 = canvas.create_image(725,175, image=imageVirus0, anchor=NW)
-    canMove1 = canvas.create_image(80,300, image=imageVirus1, anchor=NW)
+    canMove1 = canvas.create_image(60,300, image=imageVirus1, anchor=NW)
     canMove2 = canvas.create_image(370,180, image=imageVirus2, anchor=NW)
     canMove3 = canvas.create_image(430,480, image=imageVirus3, anchor=NW)
-    if number_entiVirus==8 :
-        canvas.create_text(470,50, text="YOU WON.",fill="green", font=("Purisa", 35,"bold"))
-    canvas.create_text(530,630, text="If you can kills many Virus on the time, you win else lost.",fill="blue", font=("Purisa", 18))
-    canvas.create_text(500,660, text="It is follow level be level.",fill="blue", font=("Purisa", 18))
+    if number_entiVirus==10 :
+        canvas.create_text(500,60, text="YOU WON!!!",fill="white", font=("Purisa", 50,"bold"))
+        sd_Win()
+        YouWin = False
+    else :
+        canvas.create_text(500,50, text="Enjoy to play without lost.",fill="white", font=("Purisa", 20,"bold"))
+    canvas.create_text(510,630, text="If you can pick up many AntiVirus on the grid equal 100/100, you win.",fill="blue", font=("Purisa", 18))
+    canvas.create_text(510,660, text="I am sorry to player this game is easy player becuase you cannot lose.",fill="blue", font=("Purisa", 18))
+    canvas.create_text(170,90, text="Scores : " + str(ScoreText) + "/100",fill="yellow", font=("Purisa", 18))
+    sd_RightLeftUpDown()
     arrayToDrawing()
 
 def moveDown(event):
-    global position, grid, canMove0, canMove1, canMove2, canMove3, number_entiVirus
+    global position, grid, canMove0, canMove1, canMove2, canMove3, number_entiVirus, TouchEmemy, YouWin, TouchEmemy, ScoreText
     position = findPlayerPosition(grid)
     p_column = position[1]
     p_row = position[0]
-    if grid[p_row+1][p_column]==0:
+    if grid[p_row+1][p_column]==0 and YouWin :
         grid[p_row][p_column]=0
         grid[p_row+1][p_column]=1
-    if grid[p_row+1][p_column]==2:
+    elif grid[p_row+1][p_column]==2 and YouWin :
         grid[p_row][p_column]=0
         grid[p_row+1][p_column]=1
+        ScoreText += 10
         number_entiVirus += 1
-    canvas.delete("all")
+    elif grid[p_row+1][p_column]==4 and YouWin :
+        grid[p_row][p_column]=0
+        grid[p_row+1][p_column]=1
+        TouchEmemy = False
     canvas.delete("all")
     backgound_windows=canvas.create_image(500,500,image=bg)
     canMove0 = canvas.create_image(725,175, image=imageVirus0, anchor=NW)
-    canMove1 = canvas.create_image(80,300, image=imageVirus1, anchor=NW)
+    canMove1 = canvas.create_image(60,300, image=imageVirus1, anchor=NW)
     canMove2 = canvas.create_image(370,180, image=imageVirus2, anchor=NW)
     canMove3 = canvas.create_image(430,480, image=imageVirus3, anchor=NW)
-    if number_entiVirus==8 :
-        canvas.create_text(470,50, text="YOU WON.",fill="green", font=("Purisa", 35,"bold"))
-    canvas.create_text(530,630, text="If you can kills many Virus on the time, you win else lost.",fill="blue", font=("Purisa", 18))
-    canvas.create_text(500,660, text="It is follow level be level.",fill="blue", font=("Purisa", 18))
+    if number_entiVirus==10 :
+        canvas.create_text(500,60, text="YOU WON!!!",fill="white", font=("Purisa", 50,"bold"))
+        sd_Win()
+        YouWin = False
+    else :
+        canvas.create_text(500,50, text="Enjoy to play without lost.",fill="white", font=("Purisa", 20,"bold"))
+    canvas.create_text(510,630, text="If you can pick up many AntiVirus on the grid equal 100/100, you win.",fill="blue", font=("Purisa", 18))
+    canvas.create_text(510,660, text="I am sorry to player this game is easy player becuase you cannot lose.",fill="blue", font=("Purisa", 18))
+    canvas.create_text(170,90, text="Scores : " + str(ScoreText) + "/100",fill="yellow", font=("Purisa", 18))
+    sd_RightLeftUpDown()
     arrayToDrawing()
 
 
@@ -251,14 +312,14 @@ imageWall = PhotoImage(file="wall.png")
 bg=tk.PhotoImage(file="sky1.png")
 backgound_windows=canvas.create_image(500,500,image=bg)
 imageVirus0 = PhotoImage(file="virus.png")
-imageVirus1 = PhotoImage(file="50ofVirus.png")
+imageVirus1 = PhotoImage(file="virus.png")
 imageVirus2 = PhotoImage(file="virus.png")
-imageVirus3 = PhotoImage(file="50ofVirus.png")
+imageVirus3 = PhotoImage(file="virus.png")
 entiVirus = PhotoImage(file="entiVirus.png")
 
 
 canMove0 = canvas.create_image(725,175, image=imageVirus0, anchor=NW)
-canMove1 = canvas.create_image(80,300, image=imageVirus1, anchor=NW)
+canMove1 = canvas.create_image(60,300, image=imageVirus1, anchor=NW)
 canMove2 = canvas.create_image(375,180, image=imageVirus2, anchor=NW)
 canMove3 = canvas.create_image(430,480, image=imageVirus3, anchor=NW)
 
@@ -266,13 +327,20 @@ ememyMove0()
 ememyMove1()
 ememyMove2()
 ememyMove3()
-
+def sd_RightLeftUpDown() :
+    if YouWin :
+        winsound.PlaySound("jump3.wav",winsound.SND_FILENAME | winsound.SND_ASYNC)
+def sd_Win() :
+    winsound.PlaySound("upgrade5.wav",winsound.SND_FILENAME | winsound.SND_ASYNC)
 arrayToDrawing()
 
 
 root.resizable(False,False)
-canvas.create_text(530,630, text="If you can kills many Virus on the time, you win else lost.",fill="blue", font=("Purisa", 18))
-canvas.create_text(500,660, text="It is follow level be level.",fill="blue", font=("Purisa", 18))
+canvas.create_text(510,630, text="If you can pick up many AntiVirus on the grid equal 100/100, you win.",fill="blue", font=("Purisa", 18))
+canvas.create_text(510,660, text="I am sorry to player this game is easy player becuase you cannot lose.",fill="blue", font=("Purisa", 18))
+canvas.create_text(170,90, text="Scores : " + str(ScoreText) + "/100",fill="yellow", font=("Purisa", 18))
+canvas.create_text(500,50, text="Enjoy to play without lost.",fill="white", font=("Purisa", 20,"bold"))
+
 
 
 root.mainloop()
